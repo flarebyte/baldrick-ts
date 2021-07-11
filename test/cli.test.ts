9704b6f60eb35035456d9d649700315f55d8ab1c
Olivier Huin <olih@users.noreply.github.com>
2021-07-11 08:09:00 +0100

    Adds todo

---
+++ test/cli.test.ts
@@ -0,0 +1,8 @@
+describe('CLI features', () => {
+    it.todo('provide a standardized package json')
+    it.todo('provide a standardized .gitignore')
+    it.todo('generate all the standard markdown files')
+    it.todo('provide a standardized github workflow if needed')
+    it.todo('provide a list of suggested improvements and todo')  
+  });
+  
---
+++ test/markdown.test.ts
@@ -0,0 +1,8 @@
+describe('Markdown documentation', () => {
+  it.todo('normalizes CONTRIBUTING.md')
+  it.todo('normalizes CODE_OF_CONDUCT.md')
+  it.todo('normalizes LICENSE')
+  it.todo('normalizes MAINTENANCE.md')
+  it.todo('Updates README.md with standardized chapters')
+
+});
